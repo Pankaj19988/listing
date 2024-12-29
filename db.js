a628e51bf1c31f94a576f4cdf96e53c49adc32d8
pankaj19988 <pankajmalankiya@gmail.com>
2024-12-29 16:04:24 +0530

    latest up

--- db.js
+++ db.js
@@ -2,8 +2,19 @@ require('dotenv').config()
 const mongoose = require("mongoose");
 
 const connectToMongo = async()=>{
-   await mongoose.connect('mongodb+srv://pankajmalankiyadev:3yMd64FRG7a79UDo@listing-db.fwy64.mongodb.net/listing?retryWrites=true&w=majority&appName=listing-db');
-   console.log("connected to mongodb successfuly")
+   try {
+      await mongoose.connect('mongodb+srv://pankajmalankiyadev:3yMd64FRG7a79UDo@listing-db.fwy64.mongodb.net/listing?retryWrites=true&w=majority&appName=listing-db&tls=true', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000, // Try to connect for 5 seconds
+      });
+      console.log("Connected to MongoDB successfully");
+    } catch (err) {
+      console.error("Error connecting to MongoDB:", err);
+      setTimeout(connectToMongo, 5000); // Retry after 5 seconds
+    }
+   // await mongoose.connect('mongodb+srv://pankajmalankiyadev:3yMd64FRG7a79UDo@listing-db.fwy64.mongodb.net/listing?retryWrites=true&w=majority&appName=listing-db&tls=true');
+   // console.log("connected to mongodb successfuly")
 }
 
 module.exports = connectToMongo;
