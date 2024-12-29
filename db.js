c983394e2d9c5dd444459c87bf869966a4dab2f4
pankaj19988 <pankajmalankiya@gmail.com>
2024-12-29 15:33:25 +0530

    latest

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const mongoURI =process.env.MONGO_URL
 
 const connectToMongo = async()=>{
-   await mongoose.connect(mongoURI);
+   await mongoose.connect('mongodb+srv://pankajmalankiyadev:3yMd64FRG7a79UDo@listing-db.fwy64.mongodb.net/listing?retryWrites=true&w=majority&appName=listing-db');
    console.log("connected to mongodb successfuly")
 }
 
